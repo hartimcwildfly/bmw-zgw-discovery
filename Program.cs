69547b51e0c38cbb3034109688d0f7773d2e6051
Matthias Harter <matthias.harter@ionos.com>
2020-03-04 22:00:58 +0100

    working code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BmwDiscovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            
+            var endpoint = new IPEndPoint(IPAddress.Parse("169.254.255.255"), 6811);
+            var endpoint2 = new IPEndPoint(IPAddress.Any, 6811);
+            
+            udpClient.EnableBroadcast = true;
+            
+            var discoverMessage = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x11};
+            
+            udpClient.Send(discoverMessage, discoverMessage.Length, endpoint); 
+            
+            //wait sync for answer
+            var response = udpClient.Receive(ref endpoint);  //works with endpoint or endpoint2
+            
+            Console.WriteLine(Encoding.Default.GetString(response));
+        }
+    }
+}
