01b0d61c575d6147fcc299fabadec66152567ab5
Matthias Harter <matthias.harter@ionos.com>
2020-06-14 22:27:09 +0200

    Additionally print IP instead of only MAC and VIN

--- Program.cs
+++ Program.cs
@@ -10,19 +10,19 @@ namespace BmwDiscovery
         static void Main(string[] args)
         {
             UdpClient udpClient = new UdpClient();
-            
+
             var endpoint = new IPEndPoint(IPAddress.Parse("169.254.255.255"), 6811);
-            var endpoint2 = new IPEndPoint(IPAddress.Any, 6811);
-            
+            //var endpoint = new IPEndPoint(IPAddress.Broadcast, 6811);
             udpClient.EnableBroadcast = true;
-            
+
             var discoverMessage = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x11};
-            
-            udpClient.Send(discoverMessage, discoverMessage.Length, endpoint); 
-            
+
+            udpClient.Send(discoverMessage, discoverMessage.Length, endpoint);
+
             //wait sync for answer
             var response = udpClient.Receive(ref endpoint);  //works with endpoint or endpoint2
             
+            Console.WriteLine("Endpoint IP is: " + endpoint.Address);
             Console.WriteLine(Encoding.Default.GetString(response));
         }
     }
